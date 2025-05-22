f7aac54a7ccd7608d22f2184cc2e9b7496def69b
Phuiop3 <truongtanphu05072003@gmail.com>
2025-05-22 13:37:06 +1000

    NFT created and service log

---
+++ OpenZeppelin ERC721 - 5/contracts/MyToken.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+ 
+contract FootballPlayers is ERC721URIStorage,  Ownable {
+
+    uint256 public CarCount;
+
+    struct Car {
+        address manufacturer;
+        string name;
+        string model;
+        uint256 year;
+        uint256 timeregistered;
+        
+    }
+    struct ServiceCar{
+        address ServiceProvider;
+        string name;
+        bool isapproved;
+    }
+    struct ServiceLog{
+        uint256 timeServiced;
+        string nameProvider;
+        string description;
+    }
+     mapping(uint256 => Car) public cars;
+     mapping(address => ServiceCar) public Serviceprovider;
+     mapping(uint256 => ServiceLog) public logs;
+    // Declare the event
+    event CarRegistered(uint256 CarId, address indexed manufacturer, string car_name, string car_model, uint256 year, uint256 timeregisterd);
+    event CarServiced(uint256 timeServiced , string nameProvider, string description);
+    constructor()
+        ERC721("Car for sales", "CFS")
+        Ownable(msg.sender)
+    {}
+    
+    function registerCar(
+        string memory _name,
+        string memory _model,
+        uint256 _year,
+        string memory _tokenURI
+    ) public onlyOwner {
+         CarCount++;
+
+        uint256 tokenId = CarCount;
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        cars[tokenId] = Car({
+            manufacturer: msg.sender,
+            name: _name,
+            model: _model,
+            year:_year,
+            timeregistered: block.timestamp
+        });
+        
+        emit CarRegistered(CarCount, msg.sender, _name, _model ,_year, block.timestamp);
+        
+    }
+
+    function addServiceprovider(address newStakeholder, string memory _newName) public onlyOwner {
+        Serviceprovider[newStakeholder] = ServiceCar({ ServiceProvider: newStakeholder , name : _newName, isapproved : true});
+    }
+ modifier onlyServiceProvider{
+        require(Serviceprovider[msg.sender].isapproved == true , "Not approved service provider");
+    _;
+    }
+   function logService(string memory _newDescription) public onlyServiceProvider{
+        logs[CarCount] = ServiceLog({ timeServiced : block.timestamp , description : _newDescription , nameProvider: Serviceprovider[msg.sender].name});
+     emit CarServiced(block.timestamp,  Serviceprovider[msg.sender].name , _newDescription ); 
+   }
+
+function getServiceDetails(uint256 carId) public view returns ( string memory, string memory, uint256){
+    // Check if the provided product ID is valid
+        require(carId > 0 && carId <= CarCount, "Invalid car ID");
+        ServiceLog storage log= logs[carId];
+        return (log.nameProvider, log.description , log.timeServiced);
+}
+    function getCarDetails(uint256 carId) public view returns (address, string memory, string memory, uint256, uint256 , string memory) {
+        // Check if the provided product ID is valid
+        require(carId > 0 && carId <= CarCount, "Invalid car ID");
+        
+        // Retrieve and return the details of the specified product
+        Car storage car = cars[carId];
+        return ( car.manufacturer,car.name, car.model, car.year, car.timeregistered, tokenURI(carId));
+    }
+    }
+
+   
